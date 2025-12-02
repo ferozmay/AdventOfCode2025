c23780a2f033ea948c71c8bcb3e836f77a71026b
Feroz Hassan <75996466+ferozmay@users.noreply.github.com>
2025-12-02 18:00:35 +0000

    ignore executables

--- F/day01/solution.rs
+++ F/day01/solution.rs
@@ -12,21 +12,28 @@ fn main() {
     let data = fs::read_to_string(f).expect("Failed to read file");
 
     let mut dial_pos = 50;
-    let mut count = 0;
+    let mut part1 = 0;
+    let mut part2 = 0;
 
     for line in data.lines() {
+        let steps = line[1..].parse::<i32>().unwrap();
+
         if line.starts_with("L") {
-            let steps = line[1..].parse::<i32>().unwrap();
+            let dist_to_zero = (100 - dial_pos) % 100;
+            let crossings = (steps + dist_to_zero) / 100;
+            part2 += crossings;
+
             dial_pos = (dial_pos - steps).rem_euclid(100);
         } else {
-            let steps = line[1..].parse::<i32>().unwrap();
+            let crossings = (dial_pos + steps) / 100;
+            part2 += crossings;
             dial_pos = (dial_pos + steps).rem_euclid(100);
         }
 
         if dial_pos == 0 {
-            count += 1;
+            part1 += 1;
         }
     }
-    println!("Final dial position: {}", dial_pos);
-    println!("Part 1: {}", count);
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
