a7e7359986e7e8ca5f823007aa485c8a5ab5a2e4
Feroz Hassan <75996466+ferozmay@users.noreply.github.com>
2025-12-03 03:48:05 +0000

    part 2

--- F/day02/part2.rs
+++ F/day02/part2.rs
@@ -3,34 +3,33 @@ use std::fs;
 
 fn is_invalid_id(id: i64) -> bool {
     let id_str = id.to_string();
-    if id_str.len() % 2 != 0 {
-        return false;
+    let n = id_str.len();
+    for i in 1..=n / 2 {
+        let substr = &id_str[0..i];
+        let repeats = n / i;
+        if substr.repeat(repeats) == id_str {
+            return true;
+        }
     }
-    let (split1, split2) = id_str.split_at(id_str.len() / 2);
-    split1 == split2
+    return false;
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let data = fs::read_to_string(&args[1]).unwrap();
+    let data = fs::read_to_string(env::args().nth(1).unwrap()).unwrap();
     let mut sum: i64 = 0;
 
     for range in data.split(",") {
         let parts: Vec<&str> = range.split('-').collect();
         let (low, high) = (parts[0], parts[1]);
 
-        if (low.len() == high.len()) && low.len() % 2 != 0 {
-            continue;
-        }
-
         let low_int: i64 = low.parse().unwrap();
         let high_int: i64 = high.parse().unwrap();
 
-        for n in low_int..high_int + 1 {
+        for n in low_int..=high_int {
             if is_invalid_id(n) {
                 sum += n;
             }
         }
     }
-    println!("Part 1: {}", sum);
+    println!("Part 2: {}", sum);
 }
