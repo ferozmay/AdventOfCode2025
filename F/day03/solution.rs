4fc20b4364011acd3256bb3660625b6825178822
Feroz Hassan <75996466+ferozmay@users.noreply.github.com>
2025-12-04 01:27:57 +0000

    day 3 inputs

---
+++ F/day03/solution.rs
@@ -0,0 +1,27 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let data = fs::read_to_string(env::args().nth(1).unwrap()).unwrap();
+    let mut sum = 0;
+
+    for line in data.lines() {
+        let len = line.len();
+        let (mut high1, mut high2, mut id) = (0, 0, 0);
+        for index in 0..(len - 1) {
+            let digit = line.chars().nth(index).unwrap().to_digit(10).unwrap();
+            if digit > high1 {
+                high1 = digit;
+                id = index;
+            }
+        }
+        for j in (id + 1)..(len) {
+            let digit = line.chars().nth(j).unwrap().to_digit(10).unwrap();
+            if digit > high2 {
+                high2 = digit;
+            }
+        }
+        sum += 10 * high1 + high2;
+    }
+    println!("Part 1: {}", sum);
+}
