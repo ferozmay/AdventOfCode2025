93a0877c8d5aa1c719f63326b7f1465d4f6921f4
Feroz Hassan <75996466+ferozmay@users.noreply.github.com>
2025-12-04 03:42:17 +0000

    solved part 1

---
+++ day03/part2.rs
@@ -0,0 +1,20 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let data = fs::read_to_string(env::args().nth(1).unwrap()).unwrap();
+
+    for line in data.lines() {
+        let len = line.len();
+        let (mut high1, mut high2, mut id) = (0, 0, 0);
+        let mut sum = 0;
+
+        for index in 0..len {
+            
+            for j in index..(len-12)
+
+
+        }
+
+    }
+}
