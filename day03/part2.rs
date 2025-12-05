bed4ea29b21d67726cec6115d5d12a73fb929445
Feroz Hassan <75996466+ferozmay@users.noreply.github.com>
2025-12-05 03:30:42 +0000

    solved day3 part 2

--- day03/part2.rs
+++ day03/part2.rs
@@ -1,21 +1,33 @@
 use std::env;
 use std::fs;
 
-// INCOMPLETE
 fn main() {
     let data = fs::read_to_string(env::args().nth(1).unwrap()).unwrap();
+    let mut sum = 0;
 
     for line in data.lines() {
         let len = line.len();
-        let (mut high1, mut high2, mut id) = (0, 0, 0);
-        let mut sum = 0;
+        let mut index = 0;
+        let mut num = String::new();
+        let mut to_pick = 12;
 
-        for index in 0..len {
-            
-            for j in index..(len-12)
+        while to_pick > 0 {
+            let end = len - to_pick;
+            let mut high = 0;
+            let mut found = index;
 
-
+            for i in index..=end {
+                let digit = line.chars().nth(i).unwrap().to_digit(10).unwrap();
+                if digit > high {
+                    high = digit;
+                    found = i;
+                }
+            }
+            num.push_str(&high.to_string());
+            index = found + 1;
+            to_pick -= 1;
         }
-
+        sum += num.parse::<i64>().unwrap();
     }
+    println!("Part 2: {}", sum);
 }
