ba6526f44fbed03670df6aabc6b9f4318fd4ce71
Feroz Hassan <75996466+ferozmay@users.noreply.github.com>
2025-12-08 14:14:53 +0000

    solved part 1 and 2

--- day04/solution.rs
+++ day04/solution.rs
@@ -8,18 +8,18 @@ fn main() {
 
     let rows = grid.len() as i32;
     let cols = grid[0].len() as i32;
+
     let mut accessible = 0;
     let mut removed = 0;
     let mut checked = false;
+
     loop {
         let mut to_remove = Vec::new();
-
         for r in 0..rows {
             for c in 0..cols {
                 if grid[r as usize][c as usize] != '@' {
                     continue;
                 }
-
                 let mut neighbors = 0;
                 for dr in -1..=1_i32 {
                     for dc in -1..=1_i32 {
@@ -34,17 +34,14 @@ fn main() {
                         }
                     }
                 }
-
                 if neighbors < 4 {
                     if !checked {
                         accessible += 1;
                     }
                     to_remove.push((r, c));
-                    // add this position to a hashset perhaps?
                 }
             }
         }
-
         if to_remove.is_empty() {
             break;
         }
