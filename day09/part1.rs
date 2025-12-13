cdab90a0a8ba9d4204b76e26456685b23b33a985
Feroz Hassan <75996466+ferozmay@users.noreply.github.com>
2025-12-13 17:54:26 +0000

    initial part 1

---
+++ day09/part1.rs
@@ -0,0 +1,9 @@
+use std::{env, fs};
+
+fn main() {
+    let data = fs::read_to_string(env::args().nth(1).unwrap()).unwrap();
+
+    let red_tiles: Vec<(i32, i32)> = data.lines().
+    map(|line| let coordinates: (i32, i32) = line.split(",").map(|n| n.parse().unwrap().collect())).collect();
+    println!("{:?}", data.lines());
+}
